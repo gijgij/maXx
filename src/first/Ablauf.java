310af03f1543df28a1240927b4c1ba90416956e4
julia <julia.ke811@gmail.com>
2023-01-11 17:33:31 +0100

    Spielstart-Dialog hinzugefügt

--- src/first/Ablauf.java
+++ src/first/Ablauf.java
@@ -13,12 +13,15 @@ public class Ablauf {
     public static void main(String[] args) {
 
         //Spielbeginn Dialog
-        system.out.println("Das Spiel beginnt");
+        System.out.println("Wer am schnellsten über 53 Punkte erreicht, gewinnt!!!");
+        System.out.println("Punkte können gesammelt werden, indem man sich in alle Himmelsrichtungen (N,O,S,W) bewegt.");
+        System.out.println("Ausnahmen sind: Spieler schwarz darf sich diagonal nach S-W bewegen und Spieler weiß darf sich diagonal nach N-O bewegen.");
+        System.out.println("Das Spiel beginnt");
 
         Spieler aktuellerSpieler = spiel.getPitt().getSpielerW();
 
         do {
-            //spielbrett anzeigen
+            //Spielbrett anzeigen
             for (int i = 0; i < 8; i++) {
                 String zeile = "";
                 for (int k = 0; k < 8; k++) {
@@ -27,12 +30,12 @@ public class Ablauf {
                 System.out.println(zeile);
             }
 
-            //score anzeigen
-            System.out.println("\rDer Aktuelle Score ist:");
+            //Score anzeigen
+            System.out.println("\rDer aktuelle Score ist:");
             System.out.println(spiel.getPitt().getSpielerW().getFarbe().toString() + ":  " + spiel.getPitt().getSpielerW().getPunkte() + "Punkte");
             System.out.println(spiel.getPitt().getSpielerB().getFarbe().toString() + ":  " + spiel.getPitt().getSpielerB().getPunkte() + "Punkte");
 
-            //akuteller spieler anzeigen + input erfragen + input checken und ggfs neu fragen + bewegen (intern) + Punkte hochzählen
+            //aktueller Spieler anzeigen + input erfragen + input checken und ggfs neu fragen + bewegen (intern) + Punkte hochzählen
             spiel.inputVomSpieler(aktuellerSpieler);
 
             if (aktuellerSpieler.getFarbe() == Farbe.W)
@@ -42,7 +45,7 @@ public class Ablauf {
 
         } while (aktuellerSpieler.hatGewonnen());
 
-        System.out.println("Glückwunsch! " + aktuellerSpieler.getFarbe().toString() + " hat gewonnen.");
+        System.out.println("Glückwunsch! " + aktuellerSpieler.getFarbe().toString() + " hat gewonnen!");
 
     }
 }
